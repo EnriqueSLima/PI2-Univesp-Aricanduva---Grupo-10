a2aab274e41b0064b5d4af68db4830e32cdd8a3d
snowfox <souza.enrique7@gmail.com>
2024-10-08 15:38:28 -0300

    Mudança na versão do django, para se adequar aos drivers OBDC

---
+++ v0.0.1/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/snowfox/GitHub/PI2-Univesp-Aricanduva---Grupo-10/v0.0.1/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
