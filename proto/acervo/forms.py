69d21e5ce44288cacca1e87e97ed4478dc65103e
snowfox <souza.enrique7@gmail.com>
2024-11-06 18:09:51 -0300

    Reconfigurando branch

--- proto/acervo/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Emprestimo
-
-class EmprestimoForm(forms.ModelForm):
-    class Meta:
-        model = Emprestimo
-        fields = ['livro', 'leitor', 'data_devolucao']
--- sala_leitura/sala_leitura/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SalaLeituraConfig(AppConfig):
-    name = 'sala_leitura'
