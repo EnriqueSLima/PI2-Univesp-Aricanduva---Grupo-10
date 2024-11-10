352afab6c634c0de4a9e982e16e46ac4ea915f37
snowfox <souza.enrique7@gmail.com>
2024-11-10 14:54:42 -0300

    Consolidando views de consulta

--- v0.0.1/sala_leitura/views.py
+++ v0.0.1/sala_leitura/views.py
@@ -15,77 +15,186 @@ def landing_page(request):
 def home_page(request):
     return render (request, "home_page.html")
 
+#@login_required
+#def consulta(request):
+#    livros = Livro.objects.all()
+#    alunos = Aluno.objects.all()
+#    editoras = Editora.objects.all()
+#    categorias = Categoria.objects.all()
+#
+#    return render(request, 'consulta.html', {
+#        'livros': livros,
+#        'alunos': alunos,
+#        'editoras': editoras,
+#        'categorias': categorias
+#    })
+
+#@login_required
+#def consulta_modelo(request):
+#    modelo = request.GET.get('modelo')
+#    context = {'modelo': modelo}
+#
+#    if modelo == 'livros':
+#        filtro = request.GET.get('filtro')
+#        valor = request.GET.get('valor')
+#        livros = Livro.objects.all()
+#        
+#        if filtro and valor:
+#            if filtro == 'registro':
+#                livros = livros.filter(registro__icontains=valor)
+#            elif filtro == 'autor':
+#                livros = livros.filter(autor__icontains=valor)
+#            elif filtro == 'titulo':
+#                livros = livros.filter(titulo__icontains=valor)
+#            elif filtro == 'editora':
+#                livros = livros.filter(editora__icontains=valor)
+#        
+#        context['livros'] = livros
+#
+#    elif modelo == 'alunos':
+#        filtro = request.GET.get('filtro')
+#        valor = request.GET.get('valor')
+#        alunos = Aluno.objects.all()
+#        
+#        if filtro and valor:
+#            if filtro == 'nome':
+#                alunos = alunos.filter(nome__icontains=valor)
+#            elif filtro == 'ra':
+#                alunos = alunos.filter(ra__icontains=valor)
+#            elif filtro == 'sexo':
+#                alunos = alunos.filter(sexo__iexact=valor)
+#        
+#        context['alunos'] = alunos
+#        context['is_sexo_filter'] = (filtro == 'sexo')  # Flag para o template
+#
+#    elif modelo == 'editoras':
+#        busca_nome = request.GET.get('busca_nome')
+#        editoras = Editora.objects.all()
+#        
+#        if busca_nome:
+#            editoras = editoras.filter(nome__icontains=busca_nome)
+#        
+#        context['editoras'] = editoras
+#
+#    elif modelo == 'categorias':
+#        busca_nome = request.GET.get('busca_nome')
+#        categorias = Categoria.objects.all()
+#        
+#        if busca_nome:
+#            categorias = categorias.filter(tipo__icontains=busca_nome)
+#        
+#        context['categorias'] = categorias
+#    
+#    return render(request, 'consulta.html', context)
+
 @login_required
 def consulta(request):
-    livros = Livro.objects.all()
-    alunos = Aluno.objects.all()
-    editoras = Editora.objects.all()
-    categorias = Categoria.objects.all()
-
-    return render(request, 'consulta.html', {
-        'livros': livros,
-        'alunos': alunos,
-        'editoras': editoras,
-        'categorias': categorias
-    })
+    return render(request, 'consulta.html')
 
 @login_required
-def consulta_modelo(request):
-    modelo = request.GET.get('modelo')
-    context = {'modelo': modelo}
+def lista_livros(request):
+    filtro = request.GET.get('filtro')
+    valor = request.GET.get('valor')
+    livros = Livro.objects.all()
 
-    if modelo == 'livros':
-        filtro = request.GET.get('filtro')
-        valor = request.GET.get('valor')
-        livros = Livro.objects.all()
-        
-        if filtro and valor:
-            if filtro == 'registro':
-                livros = livros.filter(registro__icontains=valor)
-            elif filtro == 'autor':
-                livros = livros.filter(autor__icontains=valor)
-            elif filtro == 'titulo':
-                livros = livros.filter(titulo__icontains=valor)
-            elif filtro == 'editora':
-                livros = livros.filter(editora__icontains=valor)
-        
-        context['livros'] = livros
+    if filtro and valor:
+        if filtro == 'registro':
+            livros = livros.filter(registro__icontains=valor)
+        elif filtro == 'autor':
+            livros = livros.filter(autor__icontains=valor)
+        elif filtro == 'titulo':
+            livros = livros.filter(titulo__icontains=valor)
+        elif filtro == 'editora':
+            livros = livros.filter(editora__icontains=valor)
+    
+    return render(request, 'lista_livros.html', {'livros': livros})
 
-    elif modelo == 'alunos':
-        filtro = request.GET.get('filtro')
-        valor = request.GET.get('valor')
-        alunos = Aluno.objects.all()
-        
-        if filtro and valor:
-            if filtro == 'nome':
-                alunos = alunos.filter(nome__icontains=valor)
-            elif filtro == 'ra':
-                alunos = alunos.filter(ra__icontains=valor)
-            elif filtro == 'sexo':
-                alunos = alunos.filter(sexo__iexact=valor)
-        
-        context['alunos'] = alunos
-        context['is_sexo_filter'] = (filtro == 'sexo')  # Flag para o template
+@login_required
+def lista_alunos(request):
+    filtro = request.GET.get('filtro')
+    valor = request.GET.get('valor')
+    alunos = Aluno.objects.all()
+    is_sexo_filter = False  # Flag para identificar se o filtro é 'sexo'
 
-    elif modelo == 'editoras':
-        busca_nome = request.GET.get('busca_nome')
-        editoras = Editora.objects.all()
-        
-        if busca_nome:
-            editoras = editoras.filter(nome__icontains=busca_nome)
-        
-        context['editoras'] = editoras
+    if filtro and valor:
+        if filtro == 'nome':
+            alunos = alunos.filter(nome__icontains=valor)
+        elif filtro == 'ra':
+            alunos = alunos.filter(ra__icontains=valor)
+        elif filtro == 'sexo':
+            alunos = alunos.filter(sexo__iexact=valor)
+            is_sexo_filter = True  # Ativa a flag para o filtro de sexo
 
-    elif modelo == 'categorias':
-        busca_nome = request.GET.get('busca_nome')
-        categorias = Categoria.objects.all()
-        
-        if busca_nome:
-            categorias = categorias.filter(tipo__icontains=busca_nome)
-        
-        context['categorias'] = categorias
+    return render(request, 'lista_alunos.html', {'alunos': alunos, 'is_sexo_filter': is_sexo_filter})
 
-    return render(request, 'consulta.html', context)
+
+@login_required
+def lista_editoras(request):
+    busca_nome = request.GET.get('busca_nome')
+    editoras = Editora.objects.all()
+
+    if busca_nome:
+        editoras = editoras.filter(nome__icontains=busca_nome)
+    
+    return render(request, 'lista_editoras.html', {'editoras': editoras})
+
+@login_required
+def lista_categorias(request):
+    busca_nome = request.GET.get('busca_nome')
+    categorias = Categoria.objects.all()
+
+    if busca_nome:
+        categorias = categorias.filter(tipo__icontains=busca_nome)
+    
+    return render(request, 'lista_categorias.html', {'categorias': categorias})
+
+@login_required
+def lista_emprestimos(request):
+    filtro = request.GET.get('filtro')
+    valor = request.GET.get('valor')
+    
+    # Inicia a consulta de empréstimos com devolução confirmada
+    historicos = Emprestimo.objects.filter(data_devolucao__isnull=False)
+
+    if filtro and valor:
+        if filtro == 'aluno':
+            historicos = historicos.filter(aluno__nome__icontains=valor)
+        elif filtro == 'titulo':
+            historicos = historicos.filter(livro__titulo__icontains=valor)
+        elif filtro == 'data_devolucao':
+            try:
+                # Tenta filtrar por data de devolução se o valor for uma data válida
+                data_devolucao = timezone.datetime.strptime(valor, "%d/%m/%Y").date()
+                historicos = historicos.filter(data_devolucao=data_devolucao)
+            except ValueError:
+                pass  # Caso não seja uma data válida, não aplica filtro
+    
+    # Carrega todas as opções de alunos e livros para o filtro
+    alunos = Aluno.objects.all()
+    livros = Livro.objects.all()
+
+    return render(request, 'lista_emprestimos.html', {
+        'historicos': historicos,
+        'alunos': alunos,
+        'livros': livros,
+        'filtro': filtro,
+        'valor': valor
+    })
+
+#def lista_livros(request):
+#    return true
+#
+#def lista_alunos(request):
+#    return true
+#
+#def lista_editoras(request):
+#    return true
+#
+#def lista_categorias(request):
+#    return true
+#
+#def lista_emprestimos(request):
+#    return true
 
 @login_required
 def cadastro(request):
@@ -127,7 +236,6 @@ def cadastro(request):
             if form_categorias.is_valid():
                 form_categorias.save()
                 return redirect('cadastro')
-        # Você pode adicionar lógica semelhante para os outros formulários
 
     # Passa todos os formulários e o modelo selecionado para o contexto
     context = {
@@ -142,7 +250,7 @@ def cadastro(request):
 
 
 
-
+#   *** Views serão deletadas   ***
 #@login_required
 #def cadastrar_livro(request):
 #    if request.method == 'POST':
@@ -191,6 +299,7 @@ def cadastro(request):
 #
 #    return render(request, 'cadastrar_categoria.html', {'form': form})
 
+
 @login_required
 def emprestimo(request):
     if request.method == 'POST':
@@ -231,38 +340,5 @@ def devolver_livro(request, emprestimo_id):
     emprestimo.save()
 
     return redirect('emprestimo')  # Substitua pelo nome correto da página de consulta
-
-@login_required
-def historico_emprestimos(request):
-    filtro = request.GET.get('filtro')
-    valor = request.GET.get('valor')
-    
-    # Inicia a consulta de empréstimos com devolução confirmada
-    historicos = Emprestimo.objects.filter(data_devolucao__isnull=False)
-
-    if filtro and valor:
-        if filtro == 'aluno':
-            historicos = historicos.filter(aluno__nome__icontains=valor)
-        elif filtro == 'titulo':
-            historicos = historicos.filter(livro__titulo__icontains=valor)
-        elif filtro == 'data_devolucao':
-            try:
-                # Tenta filtrar por data de devolução se o valor for uma data válida
-                data_devolucao = timezone.datetime.strptime(valor, "%d/%m/%Y").date()
-                historicos = historicos.filter(data_devolucao=data_devolucao)
-            except ValueError:
-                pass  # Caso não seja uma data válida, não aplica filtro
-    
-    # Carrega todas as opções de alunos e livros para o filtro
-    alunos = Aluno.objects.all()
-    livros = Livro.objects.all()
-
-    return render(request, 'emprestimo.html', {
-        'historicos': historicos,
-        'alunos': alunos,
-        'livros': livros,
-        'filtro': filtro,
-        'valor': valor
-    })
 
 
